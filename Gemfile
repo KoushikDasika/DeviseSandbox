db8fe1a5e818c8087e99b9829faf92b4dabb4656
Koushik Dasika <koushikdasika@gmail.com>
2015-05-21 14:51:13 -0400

    blha

--- Gemfile
+++ Gemfile
@@ -12,7 +12,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
+gem 'devise_token_auth'
 gem 'faker'
+gem 'omniauth'
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: '/auth'
   root 'welcome#index'
 
-  devise_for :users
+  #devise_for :users
 
   resources :users do
     resources :articles
